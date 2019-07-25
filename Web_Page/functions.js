65183b42188bec6ab919b10d95a452c02aa428c1
root <zujany@gmail.com>
2019-07-25 14:57:19 +0200

    Fix the page header

--- Web_Page/functions.js
+++ Web_Page/functions.js
@@ -1,20 +1,20 @@
 
-// Fixed Header
+// // Fixed Header
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {fixed_header()};
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {fixed_header()};
 
-// Get the header
-var header = document.getElementById("myHeader");
+// // Get the header
+// var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function fixed_header() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function fixed_header() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
--- Web_Page/index.php
+++ Web_Page/index.php
@@ -14,33 +14,35 @@
 
 <body>
 <!-- Page Header -->
-<div class="container" id="myHeader">
+<!-- <div class="container" id="myHeader"> -->
+<div class="header">
+    <div class="container">
+     	<!-- <div class="page-header">
+    		<h1>Datasec4Iot</h1>
+    	</div> -->
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Datasec4Iot</a>
+            <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button> -->
 
-	<div class="header">
-		<h1>Datasec4Iot</h1>
-	</div>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <!-- <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button> -->
-
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Download <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-            </ul>
-        </div>
-    </nav>
-
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Download <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                </ul>
+            </div>
+        </nav>
+        
+    </div>
 </div>
 
 <!-- Page content: List -->
