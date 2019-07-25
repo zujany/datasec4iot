a05b0d47a81605e8ac9829ec4912f618a6b39ba6
root <zujany@gmail.com>
2019-07-25 16:03:27 +0200

    Fix the page header

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -43,7 +43,15 @@
 
     <!-- Filters -->
     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12" id="content_column" >
-        <h4>Filters</h4>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">Filters</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>>
+        </table>
     </div>
     <!--  List -->
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" id="content_column" >
