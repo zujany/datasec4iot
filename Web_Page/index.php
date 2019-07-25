5c77d450d64ed1d68c279b04d8aeb0c860ddcd2c
root <zujany@gmail.com>
2019-07-25 15:42:28 +0200

    Fix the page header

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -42,11 +42,11 @@
 <div class="row">
 
     <!-- Filters -->
-    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12" >
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12" id="content_column" >
         <h4 id="filter">Filters</h4>
     </div>
     <!--  List -->
-    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" >
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" id="content_column" >
         <table class="table table-striped">
             <thead id="list">
             <tr>
