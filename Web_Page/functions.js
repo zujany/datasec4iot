1425abc954ff8a0aacfa8cd4fd55782acd5aa7d2
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-07-27 15:29:13 +0200

    test

--- Web_Page/functions.js
+++ Web_Page/functions.js
@@ -15,7 +15,7 @@ function download_all_selected(){
 
 	for (var i = 0; i < check_box.length; i++){
 		if (check_box[i].checked) {
-			console.log(check_box[i].parent().parent().parent().children())
+			console.log(check_box[i].parentElement.parentElement.parentElement.children)
 		}
 	}
 }
