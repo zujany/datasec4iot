0ea30a5031e55c162e886b5b4b74015a69661cd8
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-07-27 15:30:55 +0200

    test

--- Web_Page/functions.js
+++ Web_Page/functions.js
@@ -15,7 +15,7 @@ function download_all_selected(){
 
 	for (var i = 0; i < check_box.length; i++){
 		if (check_box[i].checked) {
-			console.log(check_box[i].parentElement.parentElement.parentElement.children)
+			console.log(check_box[i].parentElement.parentElement.parentElement.children[2].children[0].click())
 		}
 	}
 }
