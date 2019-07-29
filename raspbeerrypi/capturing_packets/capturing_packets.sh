cf85e4209ae9d7243236e4b8d4effcddfa94a214
root <zujany@gmail.com>
2019-07-29 18:02:15 +0200

    Scripts schedule the capture of the IoT packets

---
+++ raspbeerrypi/capturing_packets/capturing_packets.sh
@@ -0,0 +1,12 @@
+#/home/pi/Documents/capturing_packets.sh  
+
+for i in 0 1 2 
+do
+	now=$(date +"%Y%m%d_%H%M%S")
+	sudo timeout 10 tcpdump -i eth0 -w /home/pi/Documents/data/packets_$now.pcap
+done
+
+
+#Transfering the data to the server
+
+sudo rsync -avz -e 'ssh' /home/pi/Documents/data/ datasec4iot@157.159.68.44:/var/www/html/datasec4iot/Web_Page/data
---
+++ raspbeerrypi/capturing_packets/start_capturing_packets.sh
@@ -0,0 +1,3 @@
+#/home/pi/Documents/start_capturing_packets.sh 
+
+sudo at midnight -f /home/pi/Documents/capturing_packets.sh 
