1f63e5b24b65cc6376d5cfbc600b3b483c36f714
zujany <zujany@gmail.com>
2019-07-30 15:12:38 +0000

    Importing correctly jquery

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -119,6 +119,11 @@
 
 
 <!-- <script type="text/javascript" src="functions.js"></script> -->
+
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" type="text/javascript"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="Stylesheet" type="text/css" />
+
 </body>
 </html>
 
