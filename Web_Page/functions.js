dc9fa30be7b2e44035a74b3399ba40dcc08aeee9
zujany <zujany@gmail.com>
2019-07-30 15:33:00 +0000

    Comparing dates

--- Web_Page/functions.js
+++ Web_Page/functions.js
@@ -2,26 +2,16 @@
 
 // Date filter 
 function date_filter(){
-        var from_raw = new Date ($('#date-filter-from').datepicker({ dateFormat: 'dd/MM/yyyy' }).val());
-        var to = $('#date-filter-to').datepicker({ dateFormat: 'dd/MM/yyyy' }).val();
+        var from = new Date ($('#date-filter-from').datepicker({ dateFormat: 'dd/MM/yyyy' }).val());
+        var to = new Date ($('#date-filter-to').datepicker({ dateFormat: 'dd/MM/yyyy' }).val());
 
-		var from = new Date(str.concat(from_raw.getFullYear(),-,from_raw.getMonth()+1,-,from_raw.getDate())
-        console.info(from)
-
-	// if(from.getTime() == to.getTime()) {
-	//         console.info("a equals b");
-	// } 
-	// else {
-	//         console.log("a NOT equals b");
-	// }
+	 if(from.getTime() < to.getTime()) {
+	         console.info("from < to");
+	 } 
+	 else {
+	         console.log("from > to");
+	 }
 }
-
-document.getElementById("date-filter-from").addEventListener("change", function() {
-    var input = this.value;
-    var dateEntered = new Date(input);
-    console.log(input); //e.g. 2015-11-13
-    console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
-});
 
 function download_all_selected(){
 	console.log("Hello Word");
