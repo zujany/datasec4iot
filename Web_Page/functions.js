b69026a0d4f2195e4493aa5a34a240cac25fc19f
root <zujany@gmail.com>
2019-07-31 14:04:08 +0200

    Date filter

--- Web_Page/functions.js
+++ Web_Page/functions.js
@@ -27,7 +27,7 @@ function date_filter(){
 				file_id = files_dates[i].id;
 				console.log(file_id);
 
-				$('#2019-07-30').parent().hide();
+				$('#' + file_id).parent().hide();
 				// $(document).ready(function () {
 				// 	$('#file_id').parent().hide();
 				// });
