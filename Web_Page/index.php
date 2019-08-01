babdec8010876d0471076963cf8a995e0a5b1180
root <zujany@gmail.com>
2019-08-01 15:42:17 +0200

    Adding about section

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -6,7 +6,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width = device-width, initial-scale = 1">
 <title>Datasec4Iot</title>
-<!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -26,11 +25,11 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">Home</a>
               </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Download <span class="sr-only">(current)</span></a>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Download <span class="sr-only">(current)</span></a>
               </li>
             </ul>
         </div>
