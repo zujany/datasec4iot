32a19976fa0ed65cc3e48f8b57aaa4110c4a75a8
root <zujany@gmail.com>
2019-08-01 15:53:26 +0200

    Web navigation

--- Web_Page/index.php
+++ Web_Page/index.php
@@ -18,22 +18,22 @@
 <?php include 'functions.php';?>
 <!-- Page Header -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">Datasec4Iot</a>
+    <nav class="container">
+        <a class="navbar-brand" href="home.php">Datasec4Iot</a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="home.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="about.php">About</a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="#">Download <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Download <span class="sr-only">(current)</span></a>
               </li>
             </ul>
         </div>
-    </div>
+    </nav>
 </nav>
         
 
