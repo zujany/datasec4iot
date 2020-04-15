be14aba1f85f2bd0db12783867192dc531d1bb88
Zujany Salazar <45805561+zujany@users.noreply.github.com>
2020-04-15 15:18:49 +0200

    Update capturing_packets.sh

--- raspbeerrypi/capturing_packets/capturing_packets.sh
+++ raspbeerrypi/capturing_packets/capturing_packets.sh
@@ -6,7 +6,7 @@ sudo timeout 85200 tcpdump -i eth0 -w /home/pi/Documents/data/packets_$now.pcap
 
 
 #Transfering the data to the server
-sudo rsync -avz -e 'ssh' /home/pi/Documents/data/ datasec4iot@157.159.68.44:/var/www/html/datasec4iot/Web_Page/data
+sudo rsync -avz -e 'ssh' /home/pi/Documents/data/ datasec4iot@<server-ip>:/var/www/html/datasec4iot/Web_Page/data
 
 #Remove after send it to the server
 sudo rm /home/pi/Documents/data/* 
